b5fc9941db8de3bc1510ab0aa56459da2d26aa14
Alfredo <alfredo.i.urdaneta@gmail.com>
2017-03-26 18:08:19 -0400

    Setup basic structure of js modules and made temp plan sheet

---
+++ plan.js
@@ -0,0 +1,25 @@
+//Add event handler = CONTROLLER MODULE
+
+
+
+//Get input values = UI MODULE
+
+
+
+//Add the new item to our data structure = DATA MODULE
+
+
+
+//Add the new item to the UI = UI MODULE
+
+
+
+//Calculate budget = DATA MODULE
+
+
+
+//Update the UI = UI MODULE
+
+
+
+MODULE PATTERN
